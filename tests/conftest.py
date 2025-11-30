68baa2968d821d05915661d4465d9dd59fc4e374
Yusuf Emre Bayrakcı <ybayrakci04@gmail.com>
2025-11-30 16:37:53 +0300

    renaming bug fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ from security import get_db
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL}
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
